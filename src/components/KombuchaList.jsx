1e7425104dde6f362d439a76ceb20abe904fabad
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-11 13:04:17 -0500

    add map function to kombuchaList

--- src/components/KombuchaList.jsx
+++ src/components/KombuchaList.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Kombucha from "./Kombucha";
 
 function KombuchaList() {
-  return <Kombucha />;
+  return (
+    <React.Fragment>
+      <hr />
+      {masterKombuchaList.map((kombucha, index) => (
+        <Kombucha
+          name={kombucha.name}
+          brand={kombucha.brand}
+          price={kombucha.price}
+          alcoholContent={kombucha.alcoholContent}
+          flavor={kombucha.flavor}
+        />
+      ))}
+    </React.Fragment>
+  );
 }
 export default KombuchaList;
