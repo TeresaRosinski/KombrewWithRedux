050b270168d7240c03b3b85db49458d0ada362bc
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-11 15:08:12 -0500

    add NewKombuchaForm

--- src/components/NewKombuchaForm.jsx
+++ src/components/NewKombuchaForm.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { v4 } from "uuid";
 
 function NewKombuchaForm(props) {
   return (
     <React.Fragment>
-      <h3>FORMMMYY</h3>
+      <form onSubmit={handleNewKombuchaFormSubmission}>
+        <input type="text" name="name" placeholder="Kombucha Name" />
+        <input type="text" name="brand" placeholder="Kombucha Brand" />
+        <input type="text" name="flavor" placeholder="Kombucha Flavor" />
+        <input type="number" name="price" placeholder="Price Per Pint" />
+        <input
+          type="number"
+          name="alcoholContent"
+          placeholder="Alcohol Content"
+        />
+      </form>
     </React.Fragment>
   );
 }
