59561b4334587340f30c76550509492c7ce18d6e
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-11 13:38:31 -0500

    actually add updates

---
+++ src/components/KombuchaControl.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NewKombuchaForm from "./NewKombuchaForm";
+import NewKombuchaList from "./KombuchaList";
+
+class KombuchaControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <React.Fragment></React.Fragment>;
+  }
+}
+export default KombuchaControl;
---
+++ src/components/NewKombuchaForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function NewKombuchaForm(props) {
+  return (
+    <React.Fragment>
+      <h3>FORMMMYY</h3>
+    </React.Fragment>
+  );
+}
+export default NewKombuchaForm;
