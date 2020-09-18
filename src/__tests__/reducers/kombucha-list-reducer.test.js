dad4e43036346cc4ac05bed2997411751284046c
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-18 14:43:54 -0500

    add failing 'add kombucha' test

--- src/__tests__/reducers/kombucha-list-reducer.test.js
+++ src/__tests__/reducers/kombucha-list-reducer.test.js
@@ -1,7 +1,49 @@
 import kombuchaListReducer from "../../reducers/kombucha-list-reducer";
 
 describe("kombuchaListReducer", () => {
-  test("Should return default state if there is no action type passed into the reducer", () => {
-    expect(kombuchaListReducer({}, { type: null })).toEqual({});
+  let action;
+  const kombuchaData = {
+    name: "Cosmic Cranberry",
+    brand: "Intergallactic Bacteria",
+    flavor: "Cranberry",
+    price: "5",
+    alcoholContent: "5",
+    pints: 127,
+    id: 1,
+  };
+
+  test("Should successfully add new kombucha data to masterKombuchaList", () => {
+    const {
+      name,
+      brand,
+      flavor,
+      price,
+      alcoholContent,
+      pints,
+      id,
+    } = kombuchaData;
+    action = {
+      type: "ADD_KOMBUCHA",
+      name: name,
+      brand: brand,
+      flavor: flavor,
+      price: price,
+      alcoholContent: alcoholContent,
+      pints: pints,
+    };
+    expect(kombuchaListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        flavor: flavor,
+        price: price,
+        alcoholContent: alcoholContent,
+        pints: pints,
+        id: id,
+      },
+    });
   });
+});
+test("Should return default state if there is no action type passed into the reducer", () => {
+  expect(kombuchaListReducer({}, { type: null })).toEqual({});
 });
