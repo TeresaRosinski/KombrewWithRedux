5ade82b3c11bb151eef7482eb5f56ff220f0a4ce
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-18 14:47:30 -0500

    add first reducer

--- src/reducers/kombucha-list-reducer.js
+++ src/reducers/kombucha-list-reducer.js
@@ -1,3 +1,18 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, flavor, price, alcoholContent, pints, id } = action;
+  switch (action.type) {
+    case "ADD_Kombucha":
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          flavor: flavor,
+          price: price,
+          alcoholContent: alcoholContent,
+          pints: pints,
+        },
+      });
+    default:
+      return state;
+  }
 };
