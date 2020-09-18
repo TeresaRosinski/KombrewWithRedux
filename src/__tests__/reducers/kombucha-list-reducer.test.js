1d32b9189a1fa53179ead1c4f7c9900a396ba4a2
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-09-18 16:15:36 -0500

    add failing 'delete_kombucha' test

--- src/__tests__/reducers/kombucha-list-reducer.test.js
+++ src/__tests__/reducers/kombucha-list-reducer.test.js
@@ -1,6 +1,26 @@
 import kombuchaListReducer from "../../reducers/kombucha-list-reducer";
 
 describe("kombuchaListReducer", () => {
+  const currentState = {
+    1: {
+      name: "Cosmic Cranberry",
+      brand: "Intergallactic Bacteria",
+      flavor: "Cranberry",
+      price: "5",
+      alcoholContent: "5",
+      pints: 127,
+      id: 1,
+    },
+    2: {
+      name: "Sailin Strawberry",
+      brand: "Intergallactic Bacteria",
+      flavor: "Strawberry",
+      price: "5",
+      alcoholContent: "5",
+      pints: 127,
+      id: 1,
+    },
+  };
   let action;
   const kombuchaData = {
     name: "Cosmic Cranberry",
@@ -41,6 +61,24 @@ describe("kombuchaListReducer", () => {
         alcoholContent: alcoholContent,
         pints: pints,
         id: id,
+      },
+    });
+  });
+
+  test("Should Successfully delete a Kombucha", () => {
+    action = {
+      type: "DELETE_KOMBUCHA",
+      id: 1,
+    };
+    expect(kombuchaListReducer(currentState, action)).toEqual({
+      2: {
+        name: "Sailin Strawberry",
+        brand: "Intergallactic Bacteria",
+        flavor: "Strawberry",
+        price: "5",
+        alcoholContent: "5",
+        pints: 127,
+        id: 1,
       },
     });
   });
